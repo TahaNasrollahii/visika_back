cca067fa43fd6b2a2a50614c720ca33ef126eb09
raminhrd <raminhr.work@gmail.com>
2025-12-28 21:48:49 +0330

    fix celery

--- visika/celery.py
+++ visika/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodkadeh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visika.settings')
 
-app = Celery('foodkadeh')
+app = Celery('visika')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
