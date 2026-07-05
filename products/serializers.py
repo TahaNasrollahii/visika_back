7618ef705d64ddf1e7fbc7649c97d3194b695526
TaHa <tahanasrollahii@gmail.com>
2026-07-06 00:02:44 +0330

    bug fix

--- products/serializers.py
+++ products/serializers.py
@@ -30,7 +30,4 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_image(self, obj):
         if not obj.image:
             return None
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
         return obj.image.url
